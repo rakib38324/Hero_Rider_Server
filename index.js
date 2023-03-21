8127f83c37cd5c653c2a3a33969f84e8e676da6c
rakib38324 <rakib3832@gmail.com>
2023-03-22 03:08:15 +0600

    post user data

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 const app = express();
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qlqg855.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+
+        const usersCollections = client.db('Hero_Rider').collection('Users');
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollections.insertOne(user);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.log)
+
+
 
 app.get('/', async(req , res)=>{
     res.send("Hero Rider Server is running...")
